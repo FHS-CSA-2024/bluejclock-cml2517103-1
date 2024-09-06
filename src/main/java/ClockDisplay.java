65122580d472b69923eba751a7dd5a3f8cb95785
cml2517103 <cml2517103@gmail.com>
2024-09-06 09:10:33 -0700

    Update ClockDisplay.java

--- src/main/java/ClockDisplay.java
+++ src/main/java/ClockDisplay.java
@@ -1,33 +1,69 @@
+package src.main.java;
 
-public class ClockDisplay
-{
+
+public class ClockDisplay{
     //Implement 2 private NumberDisplay objects. 
     //  One for hours and one for minutes
     //Implement a private String for the display
-
+    private NumberDisplay hour;
+    private NumberDisplay minute;
+    private String display;
+    
     //Implement a constructor that takes no parameters
     //The constructor should set hours as a NumberDisplay object with 24 as the limit
     //The constructor should set minutes as a NumberDisplay object with 60 as the limit
     //The constructor should call the method updateDisplay before finishing
+    public ClockDisplay(){
+        hour = new NumberDisplay(24);
+        minute = new NumberDisplay(60);
+        updateDisplay();
+    }
     
     //Implement a constructor that takes 2 parameters, hour and minute
     //The constructor should set hours as a NumberDisplay object with 24 as the limit
     //The constructor should set minutes as a NumberDisplay object with 60 as the limit
     //The constructor should call the method setTime with the parameters passed in
+    public ClockDisplay(int h, int m){
+        hour = new NumberDisplay(24);
+        minute = new NumberDisplay(60);
+        setTime(h, m);
+    }
     
     //Implement a method timeTick that takes no parameters and returns nothing
     //The method should increase the minute value by one each run
     //The hours should increase when the minutes roll over
     //The updateDisplay method should be called before finishing
+    public void timeTick(){
+        minute.increment();
+        if(minute.getValue() == 0){
+            hour.increment();
+        }
+        updateDisplay();
+    }
     
     //Implement a method setTime that takes in 2 parameters, hour and minute
     //The method should set the hours value and minutes value
     //The updateDisplay method should be called before finishing
+    public void setTime(int h, int m){
+        hour.setValue(h);
+        minute.setValue(m);
+        updateDisplay();
+    }
     
     //Implement a method getTime that takes no parameter and returns a String
     //The return String should be formatted as HH:MM and report out the current time
-
+    public String getTime(){
+        return display;
+    }
+    
     //Implement a method updateDisplay that takes no parameters and returns nothing
     //The method should update the displayString with the current time in a format
     //  HH:MM
+    public void updateDisplay(){
+        String newDisplay = "";
+        newDisplay += hour.getDisplayValue();
+        newDisplay += ":";
+        newDisplay += minute.getDisplayValue();
+        display = newDisplay;
+    }
 }
