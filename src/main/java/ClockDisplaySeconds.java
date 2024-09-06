71c914516a5e284e707e83d337df0907488c698c
cml2517103 <cml2517103@gmail.com>
2024-09-06 09:11:23 -0700

    Update ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,55 @@
+package src.main.java;
 
-public class ClockDisplaySeconds
-{
-    //Extend ClockDisplaySeconds to include a seconds field.
-    //Update all methods and parameters to accomodate this change
+
+public class ClockDisplaySeconds{
+    private NumberDisplay hour;
+    private NumberDisplay minute;
+    private NumberDisplay second;
+    private String display;
+    
+    public ClockDisplaySeconds(){
+        hour = new NumberDisplay(24);
+        minute = new NumberDisplay(60);
+        second = new NumberDisplay(60);
+        updateDisplay();
+    }
+    
+    public ClockDisplaySeconds(int h, int m, int s){
+        hour = new NumberDisplay(24);
+        minute = new NumberDisplay(60);
+        second = new NumberDisplay(60);
+        setTime(h, m, s);
+    }
+    
+    public void timeTick(){
+        second.increment();
+        if(second.getValue() == 0){
+            minute.increment();
+        }
+        if(minute.getValue() == 0){
+            hour.increment();
+        }
+        updateDisplay();
+    }
+    
+    public void setTime(int h, int m, int s){
+        hour.setValue(h);
+        minute.setValue(m);
+        second.setValue(s);
+        updateDisplay();
+    }
+    
+    public String getTime(){
+        return display;
+    }
+    
+    public void updateDisplay(){
+        String newDisplay = "";
+        newDisplay += hour.getDisplayValue();
+        newDisplay += ":";
+        newDisplay += minute.getDisplayValue();
+        newDisplay += ":";
+        newDisplay += second.getDisplayValue();
+        display = newDisplay;
+    }
 }
