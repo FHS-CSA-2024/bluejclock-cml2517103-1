1c5ac3232f773f1ea59eb49f687aade62359a1b4
cml2517103 <cml2517103@gmail.com>
2024-09-06 09:11:01 -0700

    Update NumberDisplay.java

--- src/main/java/NumberDisplay.java
+++ src/main/java/NumberDisplay.java
@@ -1,24 +1,58 @@
+package src.main.java;
 
 
-public class NumberDisplay
-{
+
+public class NumberDisplay{
     //implement 2 private variables, limit and value.
     //  both should be ints
-
+    private int limit;
+    private int value;
     
     //implement a constructor that sets the limit to a passed in variable rollOverLimit
     //  should set value to 0
-    
+    public NumberDisplay(int rollOverLimit){
+        limit = rollOverLimit;
+        value = 0;
+    }
     //Implement getter/setter methods for limit and value
     //  The setValue method should check the input for validity
     //      (should not be less than 0, or over the limit)
-
+    public int getlimit(){
+        return limit;
+    }
+    
+    public void setLimit(int l){
+        limit = l;
+    }
+    
+    public int getValue(){
+        return value;
+    }
+    
+    public void setValue(int v){
+        if(v >= 0 && v < limit){
+            value = v;
+        }
+    }
     //Implement a function getDisplayValue that take no parameters and returns a String.
     //  The return string should be formatted as a two digit number
     //  If the number is less than 10 it should return a 0 before the number.
-
+    public String getDisplayValue(){
+        String output = "";
+        if(value < 10){
+            output += "0";
+        }
+        output += value;
+        return output;
+    }
 
     //Implement a method increment that takes no parameters and returns nothing. 
     //  This function should increase value by 1, unless it reaches the limit.
     //  It should reset to 0 if the limit is reached
+    public void increment(){
+        value ++;
+        if(value >= limit){
+            value = 0;
+        }
+    }
 }
